201f34002bf6934dfec4f347dc9a7f07a46e3ec1
Yeepers-bee <mallorydecker8220@gmail.com>
2025-02-04 10:53:38 -0500

    mm

---
+++ AbsoluteNumber.py
@@ -0,0 +1,10 @@
+user_number = int(input("Write the number: "))
+
+user_number = int(user_number)
+
+if user_number < 0:
+    user_number *= -1
+else:
+    pass
+
+print("Absolute number is: ", str(user_number))
---
+++ NegPosInt.py
@@ -0,0 +1,8 @@
+int_input = int(input("Number: "))
+
+if int_input > 0:
+    print("positive")
+if int_input < 0:
+    print("negative")
+if int_input == 0:
+    print("0")
